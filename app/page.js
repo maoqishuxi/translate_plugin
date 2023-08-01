abfcad81146c0d14398a0d364682715c8337ca65
maoshu <maoqishuxi@gmail.com>
2023-08-01 14:54:17 +0800

    粘贴事件完成2

--- app/page.js
+++ app/page.js
@@ -1,17 +1,6 @@
 'use client'
 
 
-import Image from 'next/image'
-
-
-function HandlerPaste({onPaste}) {
-  return (
-    <>
-      <textarea onPaste={onPaste} placeholder='请填写信息'></textarea>
-    </>
-  )
-}
-
 export default function Home() {
   const handlerPaste = (event) => {
     const paste = (event.clipboardData || window.clipboardData).getData("text");
@@ -21,7 +10,7 @@ export default function Home() {
   return (
     <>
       <h1>welcome to you</h1>
-      <HandlerPaste onPaste={handlerPaste} />
+      <textarea onPaste={handlerPaste}></textarea>
     </>
   )
 }

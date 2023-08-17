22a1be93b1fddc6d72199dc4ee0cddd34dc18d9b
maoshu <maoqishuxi@gmail.com>
2023-08-17 13:53:00 +0800

    时钟

--- app/page.js
+++ app/page.js
@@ -1,34 +1,42 @@
-'use client'
+"use client";
 
 import { useState, useEffect, useRef } from "react";
 
+export default function Stopwatch() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const intervalRef = useRef(null);
+  const pauseRef = useRef(0);
 
-export default function Home() {
-  const [isPlaying, setIsPlaying] = useState(false);
+  const handleStart = () => {
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+
+    console.log(pauseRef.current);
+  };
+
+  const handleStop = () => {
+    clearInterval(intervalRef.current);
+    if (startTime != null && now != null) {
+      pauseRef.current = pauseRef.current + (now - startTime) / 1000;
+    }
+  };
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = pauseRef.current + (now - startTime) / 1000;
+  }
 
   return (
     <>
-      <button onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? 'Pause' : 'Play'}
-      </button>
-      <VideoPlayer
-        isPlaying={isPlaying}
-        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-      />
+      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
     </>
-  )
-}
-
-function VideoPlayer({ src, isPlaying }) {
-  const ref = useRef(null);
-
-  useEffect(() => {
-    isPlaying ? (
-      ref.current.play()
-    ) : (
-      ref.current.pause()
-    )
-  }, [isPlaying])
-
-  return <video ref={ref} src={src} loop playsInline />;
+  );
 }

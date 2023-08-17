d042b450fd3c40f09aac2e8dec884aa31c76fabf
maoshu <maoqishuxi@gmail.com>
2023-08-17 17:37:07 +0800

    配置更新完成

--- app/page.js
+++ app/page.js
@@ -1,42 +1,11 @@
 "use client";
 
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, forwardRef } from "react";
 
-export default function Stopwatch() {
-  const [startTime, setStartTime] = useState(null);
-  const [now, setNow] = useState(null);
-  const intervalRef = useRef(null);
-  const pauseRef = useRef(0);
-
-  const handleStart = () => {
-    setStartTime(Date.now());
-    setNow(Date.now());
-
-    clearInterval(intervalRef.current);
-    intervalRef.current = setInterval(() => {
-      setNow(Date.now());
-    }, 10);
-
-    console.log(pauseRef.current);
-  };
-
-  const handleStop = () => {
-    clearInterval(intervalRef.current);
-    if (startTime != null && now != null) {
-      pauseRef.current = pauseRef.current + (now - startTime) / 1000;
-    }
-  };
-
-  let secondsPassed = 0;
-  if (startTime != null && now != null) {
-    secondsPassed = pauseRef.current + (now - startTime) / 1000;
-  }
-
+export default function Home() {
   return (
     <>
-      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
-      <button onClick={handleStart}>Start</button>
-      <button onClick={handleStop}>Stop</button>
+      <h1>welcome to you</h1>
     </>
   );
 }
